726ab734d1f34bbe7373a8398905915532386ff8
Taylher <alexmota@alu.ufc.br>
2022-12-05 16:44:36 -0300

    atualizando heroes component

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/interfaces/hero';
+import { HeroService } from 'src/app/services/hero/hero.service';
+import { MessageService } from 'src/app/services/message/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,8 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  constructor() {}
-  hero = 'Windstorm';
+  selectedHero?: Hero;
 
-  ngOnInit(): void {}
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 }
